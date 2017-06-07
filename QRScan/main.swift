03d63a96d9d91c63384ca14677bcb54f8e2a2a67
Alex Brodie <abrodie77@hotmail.com>
2017-06-07 01:23:53 -0700

    cp

--- QRScan/main.swift
+++ QRScan/main.swift
@@ -7,6 +7,40 @@
 //
 
 import Foundation
+import CoreImage
 
 print("Hello, World!")
 
+if (CommandLine.argc != 2) {
+    print("Usage: qrscan filename")
+    abort()
+}
+
+let path = CommandLine.arguments[1]
+let image = CIImage(contentsOf: URL(fileURLWithPath: path))
+if (image == nil) {
+    print("Couldn't load '\(path)'")
+    abort()
+}
+
+//let trans = CGAffineTransform(scaleX: <#T##CGFloat#>, y: <#T##CGFloat#>)
+let context = CIContext()
+let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: options)!
+let features = detector.features(in: image!)
+
+/*
+if let detector = detector {
+    let features = detector.featuresInImage(image)
+    for feature in features as! [CIQRCodeFeature] {
+        resultImage = drawHighlightOverlayForPoints(
+            image,
+            topLeft: feature.topLeft, topRight: feature.topRight,
+            bottomLeft: feature.bottomLeft, bottomRight: feature.bottomRight)
+        
+        print(feature.messageString)
+    }
+}
+*/
+
+print("Fin!")
