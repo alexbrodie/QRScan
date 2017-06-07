958490c03db48bcc65bf5194c82dc770dec4fbd0
Alex Brodie <abrodie77@hotmail.com>
2017-06-06 21:49:30 -0700

    Initial Commit

---
+++ QRScan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QRScan
+//
+//  Created by Alexander Brodie on 6/6/17.
+//  Copyright © 2017 Alexander Brodie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
