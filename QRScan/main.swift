27432c07f044979e5f862478ef4d3d4adbd8d83a
Alex Brodie <abrodie77@hotmail.com>
2017-09-15 23:52:48 -0700

    checkpoint

--- QRScan/main.swift
+++ QRScan/main.swift
@@ -23,17 +23,29 @@ if (image == nil) {
 }
 
 // Scale to fit in destSize (QR code detector doesn't work on big images)
-let destSize = CGSize(width: 500, height: 500)
+let destSize = CGSize(width: 1000, height: 1000)
 let sourceSize = image!.extent.size
-let scale = (destSize.width / destSize.height < sourceSize.width / sourceSize.height) ? (destSize.width / sourceSize.width) : (destSize.height / destSize.height)
-let trans = CGAffineTransform(scaleX: scale, y: scale)
-let scaledImage = image!.applying(trans)
+var scale = (destSize.width / destSize.height < sourceSize.width / sourceSize.height) ? (destSize.width / sourceSize.width) : (destSize.height / destSize.height)
+scale = min(scale, 1)
+
+let scanImage = image!.applying(CGAffineTransform(scaleX: scale, y: scale))
+
+/*
+let transforms: [CGAffineTransform] = [
+    CGAffineTransform(a: scale, b: 0, c: 0, d: scale, tx: 0, ty: 0), // 0 degrees
+    CGAffineTransform(a: 0, b: scale, c: -scale, d: 0, tx: 0, ty: 0), // 90 degrees
+    CGAffineTransform(a: -scale, b: 0, c: 0, d: -scale, tx: 0, ty: 0), // 180 degrees
+    CGAffineTransform(a: 0, b: -scale, c: scale, d: 0, tx: 0, ty: 0)] // 270 degrees
+
+for transform in transforms {
+    let scanImage = image!.applying(transform)
+*/
 
 // Extract QR code features
 let context = CIContext()
 let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
 let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: options)!
-let features = detector.features(in: scaledImage)
+let features = detector.features(in: scanImage)
 
 // Process each QR code found
 for feature in features as! [CIQRCodeFeature] {
